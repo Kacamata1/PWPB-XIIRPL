d9a3a68de0b4348887afbf563117d73f73c86009
Kacamata1 <fajarfaddilah@gmail.com>
2022-11-15 12:55:02 +0700

    BASDAT_CLASS

---
+++ display/css5/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>MY WEBSITE</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <div class="container">
+        <div class="header">
+            <h1 class ="judul">My Website</h1>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">product</a></li>
+                <li><a href="#">servis</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </div>
+        <div class="hero"></div>
+        <div class="content">
+            <h2>Judul Artikel</h2>
+            <p class="Penulis">Di Tulis Oleh <a href="#">Fajar</a>Pada Tanggal 15 November 2022</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est magnam eaque ex, a minima sapiente ab iusto ullam error molestias cumque ducimus voluptas quis laudantium maxime et nemo mollitia asperiores?
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequatur inventore quod voluptates vero excepturi distinctio ea officia! Expedita commodi excepturi amet molestiae quaerat pariatur, illo veritatis laudantium unde beatae!</p></p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita quia accusantium esse animi? Soluta autem a voluptate perferendis, illum voluptatem rerum omnis sunt inventore accusamus beatae est mollitia consectetur commodi?</p>        
+        </div>
+        <div class="footer">
+            <p class="copy">Copyright 2022 Fajar</p>
+        </div>
+  </div>
+</body>
+</html>
